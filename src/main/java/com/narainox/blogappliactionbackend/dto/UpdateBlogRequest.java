205847a0f90e1207fe9ed0cbafc82e555bd671b6
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2024-01-22 23:04:07 +0530

    validations Started.

--- src/main/java/com/narainox/blogappliactionbackend/dto/UpdateBlogRequest.java
+++ src/main/java/com/narainox/blogappliactionbackend/dto/UpdateBlogRequest.java
@@ -1,6 +1,8 @@
 package com.narainox.blogappliactionbackend.dto;
 
 import com.narainox.blogappliactionbackend.models.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UpdateBlogRequest {
+    @NotNull(message = "BlogId is required parameter.")
     private Integer blogId;
+    @NotBlank(message = "Title is required parameter.")
     private String title;
+    @NotBlank(message = "Description is required parameter.")
     private String description;
+    @NotNull(message = "Publish is required parameter.")
     private Boolean publish;
+    @NotNull(message = "User is required parameter.")
     private User user;
 }
